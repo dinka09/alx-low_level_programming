8502d54ecd7d3726c1ecb00cb957080f4b0b30b3
dinka09 <dinkawolde09@gmail.com>
2022-06-03 11:56:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * main - printing using in printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,\n");
+	return (0);
+}
