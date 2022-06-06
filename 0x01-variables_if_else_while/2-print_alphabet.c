a55041571b61c187f40355c56fb52f83007dad26
dinka09 <dinkawolde09@gmail.com>
2022-06-06 04:45:29 -0700

    Task 02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@ int main(void)
 {
     char alphabet = 'a';
     while (alphabet <= 'z');
-{
-    putchar(alphabet);
-    alphabet++;
+    {
+        putchar(alphabet);
+        alphabet++;
 }
-putchar('\n');
+    putchar('\n');
+    return (0);
 }
-return (0);
 
 
