f5e93790a8a8d89f5e7125134069ed654d30a520
dinka09 <dinkawolde09@gmail.com>
2022-06-06 03:28:30 -0700

    Task 02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Printing lower case alphabetics
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z');
+}
+{
+	puchar(alphabet);
+	alphabet++;
+}
+putchar ('\n');
+return (0);
+}
+
