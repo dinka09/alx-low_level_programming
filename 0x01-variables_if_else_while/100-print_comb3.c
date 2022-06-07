bfe11c4ed9e1325aa9685c67099bc16161bc329e
dinka09 <dinkawolde09@gmail.com>
2022-06-07 03:57:45 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,19 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 48; i <= 55; i++)
 	{
 	for (j = 49; j <= 56; j++)
 	{
-	for (k = 50; k <= 57; k++)
-	{
-	if (i < j && i < k && j < k)
+	if (i < j)
 	{
 	putchar(i);
 	putchar(j);
-	putchar(k);
-	if ((i < 55) || (j < 56) || (k < 57))
+	if ((i < 55) || (j < 56))
 	{
 	putchar(44);
 	putchar(32);
-	}
 	}
 	}
 	}
