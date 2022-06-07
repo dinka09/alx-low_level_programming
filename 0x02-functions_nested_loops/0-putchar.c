88a85144b26ce3234b46a02aedf9f9123109b293
dinka09 <dinkawolde09@gmail.com>
2022-06-07 12:01:03 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for(i = 0; i <= 8; i++)
+	{
+		putchar(word[i]);
+	}
+	putchar('\n');
+	return (0);
+}
