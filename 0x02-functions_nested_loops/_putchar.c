e11110ae7ca3678025cd1ffea26df95a02552598
dinka09 <dinkawolde09@gmail.com>
2022-06-08 08:22:56 -0700

    Task 01

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,25 +1,22 @@
-nclude <unistd.h>
 
-
+nclude "main.h"
 
 /**
  *
- *  * _putchar - writes the character c to stdout
+ * main - prints _putchar
  *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-
-int _putchar(char c)
-
+ * Return: 0
+ */
+int main(void)
 {
-
-		return (write(1, &c, 1));
-
+	_putchar('_');
+       	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
