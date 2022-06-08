f40a86084368d855d09beb870569d5ecd26db150
dinka09 <dinkawolde09@gmail.com>
2022-06-08 13:08:02 -0700

    Task 01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Pringitn alphabets using _putchar
+ *
+ * Return: ALways 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
+
