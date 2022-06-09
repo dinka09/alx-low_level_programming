af5f908dd8f5ffdc523e9530b419169caaa0a581
dinka09 <dinkawolde09@gmail.com>
2022-06-09 06:01:21 -0700

    Task 07

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_last_digit - Printing last digit of the gven number
+ * @n: the last digit number
+ * Return: the last digitof the given integer
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return(x);			
+}
