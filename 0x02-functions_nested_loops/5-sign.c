102813eb3b44ae0a22c8428f954383c973ddfae6
dinka09 <dinkawolde09@gmail.com>
2022-06-09 03:27:33 -0700

    Task 05

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - printing sign of the give number
+ *
+ * Return: returns 1 0 -1 based on the value of the given number
+ */
+int print_sign(int n)
+{
+	int n;
+
+	for (n > 0)
+	{
+		_putchar('+';)
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (1);
+	}
+}
