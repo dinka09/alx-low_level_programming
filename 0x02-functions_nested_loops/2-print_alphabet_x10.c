803144a38a6c7624e00f6bd688d3f58b424e5f85
dinka09 <dinkawolde09@gmail.com>
2022-06-09 02:13:43 -0700

    Task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *void print_alphabet_x10(void)- printing alphabet in loops 10xtimes
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
