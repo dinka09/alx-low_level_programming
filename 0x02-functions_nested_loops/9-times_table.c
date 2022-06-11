11d8227cb5c4603d7b0a4f14544a36013be6390c
dinka09 <dinkawolde09@gmail.com>
2022-06-11 04:09:35 -0700

    Task 08

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				else if (k >= 10)
 				{
 					_putchar(',');
-					_putchatr(' ');
+					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 					
