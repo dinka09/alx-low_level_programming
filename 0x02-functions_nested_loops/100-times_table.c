e7fc9811c22eb49a8df5fc26694489d8e0da9ce1
dinka09 <dinkawolde09@gmail.com>
2022-06-11 09:05:27 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (n = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -45,7 +45,6 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
-
 	}
 }
 
