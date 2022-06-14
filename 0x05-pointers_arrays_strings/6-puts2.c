7e51da8fb686d7d801877ad99597e94b19fd7108
dinka09 <dinkawolde09@gmail.com>
2022-06-14 06:04:35 -0700

    Task 06

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts - functions that print ever other charachters of the string starting* with the first charachter
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	 for (, str[i] != '\0' i++)
+	 {
+		 if ((i % 2) == 0)
+			 _putchar(str[i]);
+		 else
+			 continue;
+	 }
+	 _putchar('\n');
+}
