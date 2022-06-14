408e642fa896fed4a401737e5e71336d141791a5
dinka09 <dinkawolde09@gmail.com>
2022-06-14 07:12:09 -0700

    Task 07

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+/**
+ * puts_half - Printing half of the string followed by new line
+ * @str: input
+ * Return: half of the string
+ */
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	n = (len / 2);
+	if ((len % 2) == 1)
+		n = ((len + 1) / 2);
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
