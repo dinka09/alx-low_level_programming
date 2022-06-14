8a95bd0d54c22dd7e622443f1c3df748cad9b055
dinka09 <dinkawolde09@gmail.com>
2022-06-14 06:27:27 -0700

    Task 06

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,13 @@
 void puts2(char *str)
 {
 	int i = 0;
-	 for (; str[i] != '\0'; i++)
-	 {
-		 if ((i % 2) == 0)
-			 _putchar(str[i]);
-		 else
-			 continue;
-	 }
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;	
+	}
 	 _putchar('\n');
 }
