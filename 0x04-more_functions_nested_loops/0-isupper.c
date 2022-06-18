0c7650b6fb0876ac72894a2ec5da34a8d51a89fa
dinka09 <dinkawolde09@gmail.com>
2022-06-18 04:46:31 -0700

    Identifying if the given letter is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks wether the letter is uppercase or not
+ * @c: given letter
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+ {
+	 if (C >= 'A' && C <= 'Z' )
+		 return (1);
+	 else
+		 return (0);
+
+ }
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This the code for more_function more_nested_loop
