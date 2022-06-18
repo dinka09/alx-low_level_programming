f2f8fcfed2a906a37da803cbc2103cca3105b2b9
dinka09 <dinkawolde09@gmail.com>
2022-06-18 06:25:49 -0700

    function cheching digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - functions that check a digit through (0 - 9)
+ * @c: given number
+ * Return: 1 or 0
+ */
+int _isdigit(int c);
+{
+	return (c >= '0' && c <= '9');
+}
