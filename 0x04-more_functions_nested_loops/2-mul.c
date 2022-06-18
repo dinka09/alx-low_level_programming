2d89252894451b6b27dba9e74ca7641ce6c23279
dinka09 <dinkawolde09@gmail.com>
2022-06-18 06:39:51 -0700

    function that multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - functions that multiply two numbers
+ * @a: first number
+ * @b: seconf number
+ * Return: success integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
