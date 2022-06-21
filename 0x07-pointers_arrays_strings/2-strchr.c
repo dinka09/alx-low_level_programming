ebf29a5c81346832196f373987731953ca93db30
dinka09 <dinkawolde09@gmail.com>
2022-06-21 04:46:44 -0700

    first commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,2 +1,25 @@
 #include "main.h"
+/**
+ * _strchr - function that locate charachter in string
+ * @s: string
+ * @c: a character to be located in string
+ * Return: the string from which the character located
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0; b;
 
+	while (s[a])
+	{
+		a++;
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+	return ('\n');
+}
