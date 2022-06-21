ef18bcd4d2db712da78c2496decb7dd84af8f06e
dinka09 <dinkawolde09@gmail.com>
2022-06-21 06:35:45 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to char
+ * @s: the value to modify
+ * @to: the value to assing
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
