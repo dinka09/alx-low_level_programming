464b055a2ee7e885e4fd83186eab74a4e569e212
dinka09 <dinkawolde09@gmail.com>
2022-06-23 02:53:09 -0700

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - a function that return the value of x raised to y
  * @x: base value
  * @y: power value
- * Rerunr: success
+ * Return: success
  */
 int _pow_recursion(int x, int y)
 {
