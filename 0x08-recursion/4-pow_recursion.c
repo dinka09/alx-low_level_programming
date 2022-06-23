b3a19c3b183c7bab509a04f98b6538b522eaf0b4
dinka09 <dinkawolde09@gmail.com>
2022-06-23 02:49:23 -0700

    first commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that return the value of x raised to y
+ * @x: base value
+ * @Y: power value
+ * Rerunr: success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
