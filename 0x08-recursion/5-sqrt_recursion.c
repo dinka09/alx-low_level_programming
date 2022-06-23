81dc3750a259e27aa5fdcf6a1930713a7df7141f
dinka09 <dinkawolde09@gmail.com>
2022-06-23 05:08:16 -0700

    firs commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * sqrt2 - Evaluating from 1 to n
+ * @a: same number a n
+ * Return: on success 1.
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: number integer
+ * Return: on success 1
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
