2fb67f2cb643fd3b8f0c4f2d2a4481704617877b
dinka09 <dinkawolde09@gmail.com>
2022-06-27 05:58:29 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - FUnction to print its name
+ *  @argc: argc parameter
+ *  @argv: array of listed command
+ *  Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
