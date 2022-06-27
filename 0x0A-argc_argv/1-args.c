4f91bc6748d912f325fc1aa1e6ca07732aaad118
dinka09 <dinkawolde09@gmail.com>
2022-06-27 06:12:23 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Function to print the number of argument passed
+ * @argc: arg c parameter
+ * @argv: value of argc
+ * Return: 0 on success
+ */
+int main(int argc, *argv __attribute__((unused)))
+{
+	printf("%d\n", argc)
+	return (0);
+}
