55c299f81e2acfdb3ff89e382a672dc2975997b9
dinka09 <dinkawolde09@gmail.com>
2022-06-27 06:01:51 -0700

    fixed warnig

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  *  @argv: array of listed command
  *  Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
