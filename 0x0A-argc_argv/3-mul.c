7f7be737c9a836a20c2106f9d41f40a2c64f48ab
dinka09 <dinkawolde09@gmail.com>
2022-06-28 01:33:56 -0700

    first commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+	if
+	       	(argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
