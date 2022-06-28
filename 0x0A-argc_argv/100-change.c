1b9d01824849463d627caed7f6a6aa907589f7b5
dinka09 <dinkawolde09@gmail.com>
2022-06-28 05:22:02 -0700

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	num = atoi(argv[1]);
-	if (n < 0)
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while (n >= coins[j])
+		while (num >= coins[j])
 		{
 			num -= coins[j];
 			result++;
