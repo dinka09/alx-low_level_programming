69f9a2489bd04b252a8a4eb65d9742537982dcc6
dinka09 <dinkawolde09@gmail.com>
2022-06-30 06:59:04 -0700

    betty style fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = (char*)malloc(size * sizeof(char));
+	s = (char *)malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
-	return (s); 
+	return (s);
 }
