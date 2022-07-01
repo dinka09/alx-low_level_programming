2b86c47bda09e38f05ed288c286828ce87f3cce8
dinka09 <dinkawolde09@gmail.com>
2022-07-01 04:44:02 -0700

    firs commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - function returns a pointer to a newly allocated
+ * @str: size of array to be created
+ * Return: pointer to an array
+ */
+char *_strdup(char *str)
+{
+	int i = 1, j = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
