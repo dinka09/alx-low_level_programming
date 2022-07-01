80310fec15591982839af929e95aef1c2d3f90bf
dinka09 <dinkawolde09@gmail.com>
2022-07-01 05:52:52 -0700

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,17 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j])
 		j++;
+
 	l = i + j;
 	s = (char *)malloc(l * sizeof(char) + 1);
-	if ( s == NULL)
+	if (s == NULL)
 		return (NULL);
 	j = 0;
 	while (k < l)
 	{
-		if (k <= i)
+		if (k < i)
 			s[k] = s1[k];
-		if (k > i)
+		if (k >= i)
 		{
 			s[k] = s2[j];
 			j++;
