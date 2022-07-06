cc002534799792e98fae7b3688df29faecdda459
dinka09 <dinkawolde09@gmail.com>
2022-07-05 18:43:59 -0700

    first commit

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,5 +3,6 @@
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
