8f456a4baeaef2a168a6c3f634753231051a3a07
dinka09 <dinkawolde09@gmail.com>
2022-07-06 04:50:20 -0700

    first commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_iterator - ...
+ * @array: ...
+ * @size: ...
+ * @action: ...
+ *
+ * Return: ...
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
