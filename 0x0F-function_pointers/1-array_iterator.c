a9ceae177a024e810967fdc29bc699e01531c95a
dinka09 <dinkawolde09@gmail.com>
2022-07-08 18:23:33 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>                                                             
+#include <stdlib.h>
 #include <stddef.h>
 /**
  * array_iterator - ...
