d2eacfd0c4999405d1b1decc36d419b5352a1494
dinka09 <dinkawolde09@gmail.com>
2022-07-14 23:52:43 -0700

    betty style correct

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,10 +13,10 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t list_len(const list_t *h);
 int _putchar(char c);
 size_t print_list(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 
 #endif
