6cf86dbe08c3b76a0d47e8cccea98fa86705a22c
dinka09 <dinkawolde09@gmail.com>
2022-07-25 13:14:29 -0700

    added code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,6 +20,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (inlen = 0, fp = text_content; *fp; fp++)
