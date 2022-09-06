da5bd901842fe55d4f4a3160b4e6c4a2c9fe0180
dinka09 <dinkawolde09@gmail.com>
2022-09-06 06:03:07 -0700

    fixed betty style

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i / 10 != 0);
+			if (i / 10 != 0)
 			{
-				_putchar(i / 10 + '0')
+				_putchar(i / 10 + '0');
 			}
 		}
-		_putchar(i % 10 +'0')
+		_putchar(i % 10 + '0');
 	}
 }
