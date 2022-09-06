be47ce8e5a85f62f13a5d3b06ccb656ab013a25c
dinka09 <dinkawolde09@gmail.com>
2022-09-06 05:53:29 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - check for a digit (0 -9)
+ *
+ * Return: 0.
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i / 10 != 0);
+			{
+				_putchar(i / 10 + '0')
+			}
+		}
+		_putchar(i % 10 +'0')
+	}
+}
