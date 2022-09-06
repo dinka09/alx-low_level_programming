1565124b8c697b8ab218424eead71a2b84d44599
dinka09 <dinkawolde09@gmail.com>
2022-09-06 07:15:25 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - checks for a digit(0 -9)
+ * @n: variable
+ * Return: 0.
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (c = 1; x <= n; x++)
+		{
+			_putchar(95):
+		}
+	}
+	_putchar('\n');
+}
