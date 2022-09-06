950890caec634e23154b3b01fd2d68c7040de269
dinka09 <dinkawolde09@gmail.com>
2022-09-06 10:49:02 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - print
+ * @size: variable
+ * Return: 0.
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if (y <= size - x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
