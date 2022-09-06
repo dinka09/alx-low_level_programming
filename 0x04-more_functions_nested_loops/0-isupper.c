099188a933fd1c3013fb10a4876820605b6bf5d7
dinka09 <dinkawolde09@gmail.com>
2022-09-06 05:16:54 -0700

    fix error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,11 @@
  * Return: 1 or 0
  */
 int _isupper(int c)
- {
-	 if (c >= 'A' && c <= 'Z')
-	 {
-		 return (1);
-	 }
-	 return (0);
-
- }
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
 
