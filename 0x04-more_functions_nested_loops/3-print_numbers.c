9b303a662e5920e29885ec4e18e2f06847f2f963
dinka09 <dinkawolde09@gmail.com>
2022-09-06 05:22:21 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - checks for a digit (0 - 9)
+ *
+ * Return: always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+	
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
