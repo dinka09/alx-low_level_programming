44d0ba5cb50769c19ea15c4d8985822f963aeaca
dinka09 <dinkawolde09@gmail.com>
2022-09-06 11:50:43 -0700

    first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,30 @@
 #include "main.h"
 /**
- * _memset - fill memory with a constant
+ * _strcat - concatenates two strings
  *
- * @s: pointer to memory
- * @n: the number of bytes to fill
- * @b: constant byte
- * Return: a pointer to the memory area
+ * @dest: the string @src is added
+ * @src: the string to be appended
+ *
+ * Return: pointer to the resulting string @dest
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_strcat(char *dest, char *src)
 {
-	unsigned int i;
+	int len1;
+	int len2;
+	int i;
 
-	for (i = 0; i < n; i++)
+	len1 = 0;
+	len2 = 0;
+
+	while (src[len1] != '\0')
+		len1++;
+	while (dest[len2] != '\0')
+		len2++;
+
+	for (i = 0; i <= len1; i++)
 	{
-		s[i] = b;
+		dest[len2] = src[i];
+		len2++;
 	}
-	return (s);
+	return (dest);
 }
